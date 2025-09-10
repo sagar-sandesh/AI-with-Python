fbe777d037c5af697b9ac997df8a3e3f5a36730f
Sagar Sandesh Oli <sagaroli450@gmail.com>
2025-09-10 11:16:00 +0300

    Initial commit

---
+++ Assignment 02/Task04.py
@@ -0,0 +1,13 @@
+import numpy as np
+A = np.array([[1,2,3],
+              [0,1,4],
+              [5,6,0]])
+
+A_inverse = np.linalg.inv(A)
+A1 = np.dot(A, A_inverse)
+A2 = np.dot(A_inverse, A)
+
+print("Matrix A:\n", A)
+print("Inverse of matrix A:\n", A_inverse)
+print("A * A_inverse:\n", A1)
+print("A_inverse * A:\n", A2)
