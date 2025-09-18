ff3fd7efc109865f52c8ed27b4a69aabe7618bca
Sagar Sandesh Oli <sagaroli450@gmail.com>
2025-09-18 11:31:20 +0300

    Few changes has been made

---
+++ Class_work/Regression1.py
@@ -0,0 +1,25 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from sklearn.linear_model import LinearRegression
+
+my_data = np.genfromtxt("Linreg_data.csv", delimiter=",")
+xp = my_data[:,0]
+yp = my_data[:,1]
+xp = xp.reshape(-1,1)
+yp = yp.reshape(-1,1)
+regr = LinearRegression()
+regr.fit(xp, yp)
+print(regr.coef_, regr.intercept_)
+xval =np.linspace(-0.5,2,100).reshape(-1,1)
+yval =regr.predict(xval)
+plt.plot(xval,yval)
+plt.scatter(xp,yp)
+plt.show()
+
+from sklearn import metrics
+
+yhat = regr.predict(xp)
+
+
+
+
--- Class_work/Scatter plot.py
+++ Class_work/Scatter plot.py
@@ -1,28 +1,45 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import r2_score
+from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_error
+import numpy as np
+
 
 data = pd.read_csv("linreg_data.csv", header=None)
+X = data[[0]].values
+y = data[1].values
 
 
-x_col = 0
-y_col = 1
+plt.scatter(X, y, color="red", label="Data")
 
-Xpd = data[[x_col]].values
-ypd = data[y_col].values
-
-plt.scatter(Xpd, ypd, color="red", label="Data")
 
 model = LinearRegression()
-model.fit(Xpd, ypd)
-y_pred = model.predict(Xpd)
+model.fit(X, y)
 
-r2 = r2_score(ypd, y_pred)
 
-plt.plot(Xpd, y_pred, color="green", label=f"Regression Line (R²={r2:.2f})")
-plt.xlabel(f" X- Axis ")
-plt.ylabel(f" Y - Axis ")
+y_pred = model.predict(X)
+
+
+yhat = model.intercept_ + model.coef_[0] * X
+
+
+RSS = np.sum((y - y_pred) ** 2)
+MSE = mean_squared_error(y, y_pred)
+RMSE = np.sqrt(MSE)
+MAE = mean_absolute_error(y, y_pred)
+R2 = r2_score(y, y_pred)
+
+
+plt.plot(X, y_pred, color="green", label=f"Regression Line (R²={R2:.2f})")
+plt.xlabel("X - Axis")
+plt.ylabel("Y - Axis")
 plt.legend()
-plt.title(f"Scatter plot of X and Y")
+plt.title("Scatter plot of X and Y")
 plt.show()
+
+print("Values of Yhat:", y_pred)
+print("Residual Sum of Squares (RSS):", RSS)
+print("Mean Squared Error (MSE):", MSE)
+print("Root Mean Squared Error (RMSE):", RMSE)
+print("Mean Absolute Error (MAE):", MAE)
+print("R-squared (R²):", R2)
