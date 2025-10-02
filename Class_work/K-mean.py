923c813ef51ef096ef8fb6a7c1b189002657b6a9
Sagar Sandesh Oli <sagaroli450@gmail.com>
2025-10-02 12:05:06 +0300

    K-mean

---
+++ Class_work/K-mean.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn import metrics
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report
+
+
+df = pd.read_csv("iris.csv")
+
+X = df.drop("species", axis=1)
+y = df["species"]
+
+
+x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+print("Training data shape:", x_train.shape)
+print("Testing data shape:", x_test.shape)
+
+
+log_model = LogisticRegression(max_iter=200)
+log_model.fit(x_train, y_train)
+y_pred_log = log_model.predict(x_test)
+
+
+classifier = KNeighborsClassifier(n_neighbors=5)
+classifier.fit(x_train, y_train)
+y_pred_knn = classifier.predict(x_test)
+
+print("Logistic Regression Report:")
+print(classification_report(y_test, y_pred_log))
+
+
+print(" Classification Report:")
+print(classification_report(y_test, y_pred_knn))
+
+
+metrics.ConfusionMatrixDisplay.from_estimator(log_model, x_test, y_test)
+plt.title("Logistic Regression Confusion Matrix")
+plt.show()
+
+
+metrics.ConfusionMatrixDisplay.from_estimator(classifier, x_test, y_test)
+plt.title("Confusion Matrix")
+plt.show()
